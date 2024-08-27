6cd28fcaff8bc7a5b4e4c7f201ae5034ed60dea9
Hari <imamhari3001@gmail.com>
2024-08-27 14:57:27 +0700

    still error in react/three

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-
-
+import { Suspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 const Computers = () => {
+  const computers = useGLTF('./desktop_pc/scene.gltf')
+
   return (
     <mesh>
-      
+      <hemisphereLight intensity={0.15} groundColor="black" />    
     </mesh>
   )
 }
