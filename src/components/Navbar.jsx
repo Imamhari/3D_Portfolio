b8a2d0c35e4571d68253b8597a54bda7ead867e4
Hari <imamhari3001@gmail.com>
2024-08-27 13:49:28 +0700

    fixing tailwind css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,11 @@ const Navbar = () => {
             window.scrollTo(0, 0)}}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Imam Hari &nbsp;<span className='sm:block hidden'>| Full Stack Developer</span></p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+
+        </ul>
 
       </div>
     </nav>
